6c2d6d94f2b41ca77b0565e65ba06df6b38d1168
slpkid <153894144+slpkid@users.noreply.github.com>
2024-04-04 17:47:57 -0700

    Fix: Book constructor checks for checkbox status and logs correct status now.

--- default.js
+++ default.js
@@ -1,9 +1,10 @@
 const myLibrary = [];
-addTitle = document.getElementById("title")
-addAuthor = document.getElementById("author")
-addPageCount = document.getElementById("pages")
-addReadStatus = document.getElementById("read")
-addBookButton = document.getElementById("add-book-button")
+const addTitle = document.getElementById("title")
+const addAuthor = document.getElementById("author")
+const addPageCount = document.getElementById("pages")
+const addReadStatus = document.getElementById("read")
+const addBookButton = document.getElementById("add-book-button")
+const bookList = document.getElementById("book-list")
 //remove book button
 // removeBookButton = document.querySelector("")
 
@@ -11,29 +12,48 @@ function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.readStatus = read;
+    if (addReadStatus.checked) {
+        this.read = true
+    } else {
+        this.read = false
+    }
 }
 
 addBookButton.addEventListener("click", function(e) {
+    //read user input to create a new book object
     const newBook = new Book(
         addTitle.value,
         addAuthor.value,
         addPageCount.value,
         addReadStatus.value)
+        //add new book to the library array...
         myLibrary.push(newBook)
+    // visually push it now with addBookToLibrary func...
+    addBookToLibrary(newBook)
 });
+
+// placeholder books
 
 const theHobbit = new Book('The Hobbit', 'JRR Tolkien', '300', 'false');
 const fahrenheit451 = new Book('Fahrenheit 451', 'Ray Bradbury', '281', 'true');
 const shadesOfGray = new Book('Shades of Gray', 'Jasper Fforde', '1', 'false');
 
-// function addBookToLibrary() {
-    // Run Book function using text input info.
-    // add book.title to myLibrary array.
+myLibrary.push(theHobbit);
+myLibrary.push(fahrenheit451)
+myLibrary.push(shadesOfGray)
+
+// end placeholder books
+
+function addBookToLibrary(bookObj) {
     // create div with text elements, remove button, and a checkbox inside.
+    const para = document.createElement("li");
+    const node = document.createTextNode(bookObj.title + bookObj.author + bookObj.pages + bookObj.read);
+    para.appendChild(node);
+    bookList.appendChild(para);
     // fill text elements in with values title,author, pages
     // if read === true, tick checkbox, else leave it as is.
-// }
+    return
+}
 
 // function removeBook(book) {
     //  check current book's position in array...
